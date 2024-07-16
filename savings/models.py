4d2f9db2ff016ac317c03c05982e08a48dc1dbb1
Mary-Vivian <muthonimaryvivian>
2024-07-16 17:31:53 +0300

    made changes on the saving apps

--- savings/models.py
+++ savings/models.py
@@ -6,6 +6,7 @@ class Saving(models.Model):
     farmer_id_number = models.ForeignKey('farmer.Farmer', on_delete=models.CASCADE)  
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     saving_date = models.DateTimeField(default=timezone.now)
+    saving_rate=models.FloatField()
 
     def __str__(self):
         return f' {self.amount}'  
